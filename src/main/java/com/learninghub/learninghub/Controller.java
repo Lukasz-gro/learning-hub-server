6b91f0a8b382805fc5bdda3169bd3fac35beadb9
Lukasz Grobelczyk <lukaszgrobelczyk@gmail.com>
2023-03-04 12:01:23 +0100

    Add cross origin

--- src/main/java/com/learninghub/learninghub/Controller.java
+++ src/main/java/com/learninghub/learninghub/Controller.java
@@ -1,5 +1,6 @@
 package com.learninghub.learninghub;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,11 +8,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/")
     public String get() {
+        System.out.println("Default call running");
         return "I'm server!";
     }
 
+    @CrossOrigin(origins = "https://learning-hub-ui.vercel.app")
     @GetMapping("/v1/health-check")
     public String health() {
+        System.out.println("Health-check running");
         return "Healthy";
     }
 }
