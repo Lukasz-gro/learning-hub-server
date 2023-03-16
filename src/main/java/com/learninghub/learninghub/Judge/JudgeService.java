676cedb112a7f17406baad9ec72c4988689e0ea3
Lukasz Grobelczyk <lukaszgrobelczyk@gmail.com>
2023-03-16 17:01:36 +0100

    Add Judge Servie

---
+++ src/main/java/com/learninghub/learninghub/Judge/JudgeService.java
@@ -0,0 +1,28 @@
+package com.learninghub.learninghub.Judge;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+class JudgeParam {
+    String code;
+    int problemId;
+    boolean baseTestCase;
+    //TODO enum
+    String language;
+
+    @Override
+    public String toString() {
+        return code + " " + language;
+    }
+}
+
+@RestController
+public class JudgeService {
+
+    @PostMapping("/v1/judge/run-code")
+    public String runCode(@RequestBody String code) {
+        System.out.println(code);
+        return "I'm judge!";
+    }
+}
