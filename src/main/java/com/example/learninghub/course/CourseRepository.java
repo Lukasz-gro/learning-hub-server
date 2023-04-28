1feae7be7878923ab0e4aa45ff3ff1d1ec7f4a1b
MajkeIos <michals2603@gmail.com>
2023-04-28 17:20:45 +0200

    java course repository and database table added

---
+++ src/main/java/com/example/learninghub/course/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.example.learninghub.course;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+}
--- src/main/resources/setup/database/create.sql
+++ src/main/resources/setup/database/create.sql
@@ -1,4 +1,13 @@
 DROP TABLE IF EXISTS problem;
+DROP TABLE IF EXISTS course;
+
+CREATE TABLE course (
+    id              SERIAL,
+    name            TEXT NOT NULL,
+    description     TEXT NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE(name)
+);
 
 CREATE TABLE problem (
     id      SERIAL,
