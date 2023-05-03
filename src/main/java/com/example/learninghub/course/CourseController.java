3372cc8eeaa65ddcec7a56f3f67ff88e7cd1555c
MajkeIos <michals2603@gmail.com>
2023-05-03 10:49:37 +0200

    Endpoints for getting courses, problems and submits added

--- src/main/java/com/example/learninghub/course/CourseController.java
+++ src/main/java/com/example/learninghub/course/CourseController.java
@@ -1,7 +1,9 @@
 package com.example.learninghub.course;
 
+import com.example.learninghub.problem.Problem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +24,20 @@ public class CourseController {
     public String get() {
         return "I'm course!";
     }
+
+    @GetMapping("{courseId}")
+    public Course getCourse(@PathVariable("courseId") Integer id) {
+        return courseService.getCourse(id);
+    }
+
+    @GetMapping("all")
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("{courseId}/problems")
+    public List<Problem> getCourseProblems(@PathVariable("courseId") Integer id) {
+        return courseService.getCourseProblems(id);
+    }
+
 }
--- src/main/java/com/example/learninghub/problem/ProblemController.java
+++ src/main/java/com/example/learninghub/problem/ProblemController.java
@@ -3,6 +3,7 @@ package com.example.learninghub.problem;
 import com.example.learninghub.test.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class ProblemController {
     @GetMapping
     public String get() {
         return "I'm problem!";
+    }
+
+    @GetMapping("{problemId}")
+    public Problem getProblem(@PathVariable("problemId") Integer id) {
+        return problemService.getProblem(id);
     }
 }
