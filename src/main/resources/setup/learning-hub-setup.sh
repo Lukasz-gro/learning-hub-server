4920a4e5aa606a569718f046b6df1d0db5c37071
MajkeIos <michals2603@gmail.com>
2023-05-04 09:35:29 +0200

    Readme and setup directory files updated

---
+++ src/main/resources/setup/learning-hub-setup.sh
@@ -0,0 +1,13 @@
+# this script sets essential environment variables for learning-hub-server
+
+export SERVER_PORT=8080
+
+### DATABASE ###
+export DB_IP=
+export DB_PORT=
+export DB_NAME=
+export DB_USERNAME=
+export DB_PASSWORD=
+
+### DOCKER ###
+export DOCKER_DIR_PATH=/path/to/project/directory/src/main/resources/docker/
