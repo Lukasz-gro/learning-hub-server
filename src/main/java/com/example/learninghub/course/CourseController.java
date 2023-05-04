ed1e93fa515266e4dcc91e3b2790ab3462bf7cfe
MajkeIos <72340696+MajkeIos@users.noreply.github.com>
2023-05-04 09:51:51 +0200

    Basic endpoints and readme update (#10)
    * Submit logic added
    
    * Endpoints for getting courses, problems and submits added
    
    * Readme and setup directory files updated
    
    * Unnecessary endpoints removed

--- src/main/java/com/example/learninghub/course/CourseController.java
+++ src/main/java/com/example/learninghub/course/CourseController.java
@@ -1,7 +1,9 @@
 package com.example.learninghub.course;
 
+import com.example.learninghub.problem.Problem;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,19 @@ public class CourseController {
         this.courseService = courseService;
     }
 
-    @GetMapping
-    public String get() {
-        return "I'm course!";
+    @GetMapping("{courseId}")
+    public Course getCourse(@PathVariable("courseId") Integer id) {
+        return courseService.getCourse(id);
     }
+
+    @GetMapping("all")
+    public List<Course> getAllCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @GetMapping("{courseId}/problems")
+    public List<Problem> getCourseProblems(@PathVariable("courseId") Integer id) {
+        return courseService.getCourseProblems(id);
+    }
+
 }
