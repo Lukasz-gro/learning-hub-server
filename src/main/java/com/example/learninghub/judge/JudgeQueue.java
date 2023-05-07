7dc934110bfd184dc24d241de339d459c46abdc3
Filip Jasiński <72314841+FiIipson@users.noreply.github.com>
2023-05-07 21:37:51 +0200

    Queue changed and TLE handled (#15)
    * Queue changed and TLE handled
    
    * Queue changed and TLE/RTE handled
    
    * Queue changed and TLE/RTE handled

--- src/main/java/com/example/learninghub/judge/JudgeQueue.java
+++ src/main/java/com/example/learninghub/judge/JudgeQueue.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Data
 @Service
@@ -15,25 +17,56 @@ public class JudgeQueue {
     private final JudgeService judgeService;
     private final SubmitService submitService;
     private final TestService testService;
+    private final LinkedBlockingQueue<Integer> linkedBlockingQueue;
 
     @Autowired
     JudgeQueue(JudgeService judgeService, SubmitService submitService, TestService testService) {
         this.judgeService = judgeService;
         this.submitService = submitService;
         this.testService = testService;
+        linkedBlockingQueue = new LinkedBlockingQueue<>(1);
     }
 
 
     public void enqueue(JudgeParams judgeParams, Integer submitID) {
         new Thread(() -> {
+            try {
+                linkedBlockingQueue.put(submitID);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             String input = testService.getTests(judgeParams.getProblemId()).get(judgeParams.getTestCase() - 1).getInput();
             String output = testService.getTests(judgeParams.getProblemId()).get(judgeParams.getTestCase() - 1).getOutput();
 
-            String userOutput = judgeService.runCode(judgeParams, submitID, input);
+            Thread t1 = new Thread(() -> {judgeService.runCode(judgeParams, submitID, input);});
+            t1.start();
+            try {
+                t1.join(15000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println("Thread joined");
 
-            String[] outputArr = output.split("\\s+");
-            String[] userOutputArr = userOutput.split("\\s+");
-            submitService.updateSubmit(submitID, Arrays.equals(outputArr, userOutputArr) ? Status.OK : Status.ANS);
+            if (linkedBlockingQueue.peek().equals(submitID)) {
+                linkedBlockingQueue.poll();
+            }
+
+            if (t1.isAlive()) {
+                t1.interrupt();
+                System.out.println("TLE");
+                submitService.updateSubmit(submitID, Status.TLE);
+            } else {
+                String userOutput = judgeService.runCode(judgeParams, submitID, input);
+
+                String[] outputArr = output.split("\\s+");
+                String[] userOutputArr = userOutput.split("\\s+");
+                if (Objects.equals(userOutputArr[0], "RTE?*#.")) {
+                    System.out.println("RTE");
+                    submitService.updateSubmit(submitID, Status.RTE);
+                } else {
+                    submitService.updateSubmit(submitID, Arrays.equals(outputArr, userOutputArr) ? Status.OK : Status.ANS);
+                }
+            }
         }).start();
     }
 }
