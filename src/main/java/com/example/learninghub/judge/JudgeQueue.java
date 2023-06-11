ca013342d33d1b609b51889dac490b61539aaab5
FiIipson <filip.jasinski@student.uj.edu.pl>
2023-06-11 23:44:53 +0200

    multithreaded tests

--- src/main/java/com/example/learninghub/judge/JudgeQueue.java
+++ src/main/java/com/example/learninghub/judge/JudgeQueue.java
@@ -7,86 +7,121 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
 @Data
 @Service
 public class JudgeQueue {
-    private final JudgeService judgeService;
     private final SubmitService submitService;
     private final TestService testService;
     private final LinkedBlockingQueue<Integer> linkedBlockingQueue;
 
     @Autowired
-    JudgeQueue(JudgeService judgeService, SubmitService submitService, TestService testService) {
-        this.judgeService = judgeService;
+    JudgeQueue(SubmitService submitService, TestService testService) {
         this.submitService = submitService;
         this.testService = testService;
-        linkedBlockingQueue = new LinkedBlockingQueue<>(1);
+        linkedBlockingQueue = new LinkedBlockingQueue<>(10);
     }
 
 
     public void enqueueSubmit(JudgeParams judgeParams, Integer submitID) {
-        for (int i = 0; i < testService.getTests(judgeParams.getProblemId()).size(); i++) {
-            enqueue(judgeParams, submitID, i);
+        int tests = testService.getTests(judgeParams.getProblemId()).size();
+        final Status[] statuses = new Status[tests];
+        final String[] messages = new String[tests];
+        Thread[] threads = new Thread[tests];
+
+        for (int i = 0; i < tests; i++) {
+            final int testCase = i;
+            threads[i] = new Thread(() -> {enqueue(judgeParams, submitID, testCase, statuses, messages);});
+            threads[i].start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        int idRTE = indexOfStatus(statuses, Status.RTE);
+        int idANS = indexOfStatus(statuses, Status.ANS);
+        int idTLE = indexOfStatus(statuses, Status.TLE);
+
+        if (idRTE != -1) {
+            submitService.updateErrorMessage(submitID, messages[idRTE]);
+            submitService.updateSubmit(submitID, Status.RTE);
+        } else if (idANS != -1) {
+            submitService.updateOutput(submitID, messages[idANS]);
+            submitService.updateSubmit(submitID, Status.ANS);
+        } else if (idTLE != -1) {
+            submitService.updateSubmit(submitID, Status.TLE);
+        } else {
+            submitService.updateSubmit(submitID, Status.OK);
         }
     }
 
-    private void enqueue(JudgeParams judgeParams, Integer submitID, Integer testCase) {
-        new Thread(() -> {
-            try {
-                linkedBlockingQueue.put(submitID);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+    private int indexOfStatus(Status[] statuses, Status status) {
+        for (int i = 0; i < statuses.length; i++) {
+            if (statuses[i].equals(status)) {
+                return i ;
             }
+        }
+        return -1;
+    }
 
-            String input = testService.getTests(judgeParams.getProblemId()).get(testCase).getInput();
-            String output = testService.getTests(judgeParams.getProblemId()).get(testCase).getOutput();
-            final String[] userOutput = {null};
+    private void enqueue(JudgeParams judgeParams, Integer submitID, Integer testCase, Status[] statuses, String[] messages) {
+        try {
+            linkedBlockingQueue.put(submitID + testCase);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-            Thread t1 = new Thread(() -> {
-                userOutput[0] = judgeService.runCode(judgeParams, submitID, input);
-            });
+        String input = testService.getTests(judgeParams.getProblemId()).get(testCase).getInput();
+        String output = testService.getTests(judgeParams.getProblemId()).get(testCase).getOutput();
+        final String[] userOutput = {null};
+        JudgeService judgeService = new JudgeService();
 
-            try {
-                t1.start();
-                t1.join(15000);
-                System.out.println("Thread joined");
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        Thread t1 = new Thread(() -> {
+            userOutput[0] = judgeService.runCode(judgeParams, submitID, input, testCase);
+        });
 
-            if (linkedBlockingQueue.peek().equals(submitID)) {
-                linkedBlockingQueue.poll();
-            }
+        try {
+            t1.start();
+            t1.join(15000);
+            System.out.println("Thread joined");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
-            String userOutputVal = userOutput[0];
+        String userOutputVal = userOutput[0];
 
-            if (t1.isAlive()) {
-                t1.interrupt();
-                judgeService.stopContainer();
-                System.out.println("TLE");
-                submitService.updateSubmit(submitID, Status.TLE);
+        if (t1.isAlive()) {
+            t1.interrupt();
+            judgeService.stopContainer();
+            System.out.println("TLE");
+            statuses[testCase] = Status.TLE;
+        } else {
+            String[] outputArr = output.split("\\s+");
+            String[] userOutputArr = userOutputVal.split("\\s+");
+            StringBuilder sb = new StringBuilder();
+
+            if (userOutputArr.length > 0 && userOutputArr[0].equals("RTE?*#.")) {
+                messages[testCase] = userOutputVal.substring(8);
+                statuses[testCase] = Status.RTE;
             } else {
-                String[] outputArr = output.split("\\s+");
-                String[] userOutputArr = userOutputVal.split("\\s+");
-                StringBuffer sb = new StringBuffer();
-
-                if (userOutputArr.length > 0 && userOutputArr[0].equals("RTE?*#.")) {
-                    submitService.updateErrorMessage(submitID, userOutputVal.substring(8));
-                    submitService.updateSubmit(submitID, Status.RTE);
-                } else {
-                    for(int i = 0; i < userOutputArr.length; i++) {
-                        sb.append(userOutputArr[i]);
-                        sb.append(" ");
-                    }
-
-                    submitService.updateOutput(submitID, sb.toString());
-                    submitService.updateSubmit(submitID, Arrays.equals(outputArr, userOutputArr) ? Status.OK : Status.ANS);
+                for (String s : userOutputArr) {
+                    sb.append(s);
+                    sb.append(" ");
                 }
+                messages[testCase] = sb.toString();
+                statuses[testCase] = Arrays.equals(outputArr, userOutputArr) ? Status.OK : Status.ANS;
             }
-        }).start();
+        }
+
+        if (linkedBlockingQueue.peek().equals(submitID + testCase)) {
+            linkedBlockingQueue.poll();
+        }
     }
 }
